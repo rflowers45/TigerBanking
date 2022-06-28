5d1d727b8dede63e36c287032ec64a6c16e08d14
kayleedockter <kayleedockter@mail.weber.edu>
2022-06-28 08:38:10 -0600

    Trying to get deposit to work with account type. See HomeController, Deposit, and AccountVM

--- TigerBank/Models/ViewModels/AccountVM.cs
+++ TigerBank/Models/ViewModels/AccountVM.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -6,13 +7,13 @@ namespace TigerBank.Models.ViewModels
 {
     public class AccountVM : PageModel
     {
-        public Accounts Account { get; set; } 
+        public Accounts Account { get; set; } = default!;
 
         [ValidateNever]
-        public IEnumerable<SelectListItem> UsersList { get; set; }
+        public IEnumerable<SelectListItem> UsersList { get; set; } = Enumerable.Empty<SelectListItem>();
 
         [ValidateNever]
-        public IEnumerable<SelectListItem> AccountTypeList { get; set; }
+        public IEnumerable<SelectListItem> AccountTypeList { get; set; } = Enumerable.Empty<SelectListItem>();
     }
 
 }
